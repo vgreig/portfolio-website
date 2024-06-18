a1e3a220f0a17116bc772fa2c7ea188ecdc2d560
Vicky Greig <vickygreig@hotmail.co.uk>
2024-06-18 16:20:09 +0100

    correct JS file

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ fetch(`projects.json`).then(function (res) {
     data.projects.forEach(function(project){
         console.log(project.name)
         target.innerHTML += `
-            <div class="project-1">
-                    <h2>${project.name}</h2>
-                    <img src="${project.img}" alt="portfolio screenshot"/>
-                    <p class="project-text">${project.desc}</p>
-                    <ul class="skills">
-                        <li>${project.skills}</li>
-                    </ul>
-                    <p>Github<a href="${project.github}"><i class="fa-brands fa-github"></i></a></p>
-                </div>
                 <div class="project-2">
                     <h2>${project.name}</h2>
                     <img src="${project.img}" alt="portfolio screenshot"/>
@@ -25,17 +16,7 @@ fetch(`projects.json`).then(function (res) {
                     </ul>
                     <p>Github<a href="${project.github}"><i class="fa-brands fa-github"></i></a></p>
                 </div>
-                <div class="project-3">
-                    <h2>${project.name}</h2>
-                    <img src="${project.img}" alt="portfolio screenshot"/>
-                    <p class="project-text">${project.desc}</p>
-                    <ul class="skills">
-                        <li>${project.skills}</li>
-                    </ul>
-                    <p>Github<a href="${project.github}"><i class="fa-brands fa-github"></i></a></p>
-                </div>
+            
             `
-
-
     })
 })
