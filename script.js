da286aa0acb33d67eaffb812a74b5093d613bd44
Vicky Greig <vickygreig@hotmail.co.uk>
2024-06-18 16:12:39 +0100

    format JS files

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-fetch(projects.json).then(function (res) {
+fetch(`projects.json`).then(function (res) {
     return res.json()
 }).then(function (data){
     console.log(data)
-    const target = document.querySelector('.target')
+    const target = document.querySelector('.portfolio')
 
     data.projects.forEach(function(project){
         console.log(project.name)
-        target.innerHTML +=
+        target.innerHTML += `
+            <div class="project-1">
+                    <h2>${project.name}</h2>
+                    <img src="${project.img}" alt="portfolio screenshot"/>
+                    <p class="project-text">${project.desc}</p>
+                    <ul class="skills">
+                        <li>${project.skills}</li>
+                    </ul>
+                    <p>Github<a href="${project.github}"><i class="fa-brands fa-github"></i></a></p>
+                </div>
+                <div class="project-2">
+                    <h2>${project.name}</h2>
+                    <img src="${project.img}" alt="portfolio screenshot"/>
+                    <p class="project-text">${project.desc}</p>
+                    <ul class="skills">
+                        <li>${project.skills}</li>
+                    </ul>
+                    <p>Github<a href="${project.github}"><i class="fa-brands fa-github"></i></a></p>
+                </div>
+                <div class="project-3">
+                    <h2>${project.name}</h2>
+                    <img src="${project.img}" alt="portfolio screenshot"/>
+                    <p class="project-text">${project.desc}</p>
+                    <ul class="skills">
+                        <li>${project.skills}</li>
+                    </ul>
+                    <p>Github<a href="${project.github}"><i class="fa-brands fa-github"></i></a></p>
+                </div>
+            `
 
-        
+
     })
 })
